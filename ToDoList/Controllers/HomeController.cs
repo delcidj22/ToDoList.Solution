4d99eddf0880a146b41c169861031587e2d96791
delcidj22 <jdelcid23@gmail.com>
2022-03-27 11:02:54 -0700

    Update controllers to new lesson

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,16 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace ToDoList.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
-    }
 
-  }
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
 }
